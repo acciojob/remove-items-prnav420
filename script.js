535b78dbb2757187764221a2ba46198d15eaf841
prnav420 <118669489+prnav420@users.noreply.github.com>
2023-05-26 11:17:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+function remove() {
+  let option = document.getElementById("colorSelect");
+  let selectedoptions = option.options[option.selectedIndex];
+
+  if (selectedoptions.value === "Red") {
+    option.remove(option.selectedIndex);
+  } else if (selectedoptions.value === "Green") {
+    option.remove(option.selectedIndex);
+  } else if (selectedoptions.value === "White") {
+    option.remove(option.selectedIndex);
+  } else if (selectedoptions.value === "Black") {
+    option.remove(option.selectedIndex);
+  }
+}
